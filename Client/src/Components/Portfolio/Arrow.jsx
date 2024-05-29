c2f088e7ab0deab25ef20d1b5a11b783c9c18798
Tino1017 <tmagobeya1@gmail.com>
2024-05-29 15:50:54 +0200

    arrow fix

--- Client/src/Components/Portfolio/Arrow.jsx
+++ Client/src/Components/Portfolio/Arrow.jsx
@@ -4,7 +4,7 @@ import { HashLink as Link } from 'react-router-hash-link';
 
 export const Arrow = () => {
   return (
-   <div className='flex justify-center items-center h-[100px] sm:h-[40px] animate-pulse '>
+   <div className='flex justify-center items-center h-[20px] sm:h-[40px] animate-pulse '>
     <Link to='#about' className='text-secondary text-center' ><MdKeyboardDoubleArrowDown fontSize={25}/></Link>
    </div>
   )
